74320e924fef61f43c16c954b7e8727f3d0eeab0
shivachandrakante <k.s9908725092@gmail.com>
2022-07-16 22:37:10 +0530

    Added Board

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const difficultyLevel = document.getElementById("difficulty-Level")
+const mineSweeperBoard = document.getElementById("mineSweeperBoard")
+let gridboard = []
+difficultyLevel.addEventListener('change',function() {
+    console.log(difficultyLevel.value)
+    if(difficultyLevel.value === "Easy"){
+        formBoard(10)
+    } else if(difficultyLevel.value === "Medium" ){
+        formBoard(15)
+    } else{
+        formBoard(20)
+    }
+    
+})
+
+function formBoard(level){
+    while (mineSweeperBoard.hasChildNodes()) {
+        mineSweeperBoard.removeChild(mineSweeperBoard.firstChild);
+      }
+    gridboard = []
+    const numberOfBombs = (level*level) * 0.2;
+    let bombsLocation = []
+    for(let i=0;i<numberOfBombs;i++){
+        let num = Number(Math.random()*(level*level)).toFixed(0);
+        while(bombsLocation.includes(num)){
+            num = Number(Math.random()*(level*level)).toFixed(0);
+        }
+        bombsLocation.push(num)
+    }
+    let gridArrray = Array(level*level).fill('Valid')
+    for(let index of bombsLocation){
+        gridArrray[index] = "bomb"
+    }
+    let size = mineSweeperBoard.offsetWidth / level
+    console.log(size)
+    console.log(gridArrray)
+    for(let i=0;i<level*level;i++){
+        const singleBlock = document.createElement('div')
+        singleBlock.setAttribute('id',i)
+        singleBlock.classList.add(gridArrray[i])
+        singleBlock.style.width = `${size}px` 
+        singleBlock.style.height = `${size}px` 
+        mineSweeperBoard.appendChild(singleBlock)
+        gridboard.push(singleBlock)
+    }
+}
