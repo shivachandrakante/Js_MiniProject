962e98d932f85a1aef65858deaf728ba69904272
shivachandrakante <k.s9908725092@gmail.com>
2022-07-17 16:04:08 +0530

    FindBombs added

--- index.js
+++ index.js
@@ -41,7 +41,42 @@ function formBoard(level){
         singleBlock.classList.add(gridArrray[i])
         singleBlock.style.width = `${size}px` 
         singleBlock.style.height = `${size}px` 
+        if(gridArrray[i]  === "bomb" ){
+            singleBlock.style.backgroundColor = "orange";
+        } 
         mineSweeperBoard.appendChild(singleBlock)
         gridboard.push(singleBlock)
+    }
+    //  |   up left     |   up      |   up right        |
+    //  |   left        |   i       |   right           |
+    //  |   bottom left |   bottom  |   bottom right    |
+    for(let i=0 ; i < gridboard.length ; i++ ) {
+        let totalBombsAdjacentToI = 0;
+        const isLeftColumn = i % level == 0;
+        const isRightColumn = ( ( i%level ) == level - 1) ;
+        const isTopRow = ( i < level);
+        const isBottomRow =  ( i >= (level*(level-1)));
+
+
+        if(gridboard[i].classList.contains('Valid')){
+            
+            // //  left
+            // if( i>0 && !isLeftEdge && gridboard[i-1].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // //  up right
+            // if( i>level-1 && !isRightEdge && gridboard[i+1-level].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // //  up
+            // if( i>level && gridboard[i-level].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // //  up left
+            // if( i>level+1 && !isleftEdge && gridboard[i-1-level].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // //  right
+            // if( i%level!=level-1 && !isRightEdge && gridboard[i+1].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // // bottom
+            // if( i<(level*(level-1)) && !isRightEdge && gridboard[i+1+level].classList.contains('bomb')) totalBombsAdjacentToI++;
+            // // bottom left
+            // if( i<(level*(level-1)) && !isRightEdge && gridboard[i+1+level].classList.contains('bomb')) totalBombsAdjacentToI++;
+            gridboard[i].setAttribute('data',totalBombsAdjacentToI); 
+            console.log(gridboard[i])
+        }
+
     }
 }
